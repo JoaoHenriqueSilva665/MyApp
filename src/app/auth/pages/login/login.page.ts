39126b21856165b48276ee81fe71239f7804fa23
JoaoHenriqueSilva665 <proerd2010@gmail.com>
2019-09-08 11:29:53 -0300

    criado

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -8,7 +8,16 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 })
 export class LoginPage implements OnInit {
 
-  authForm: FormGroup
+  authForm: FormGroup;
+
+  configs = {
+    isSignIn: true,
+    action: 'Login',
+    actionChange: 'Create a account'
+
+  }
+
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)])
 
   constructor(private fb: FormBuilder,) { }
 
@@ -23,12 +32,24 @@ export class LoginPage implements OnInit {
     });
   }
 
+  get name(): FormControl{
+    return <FormControl>this.authForm.get('name');
+  }
+
   get email(): FormControl{
     return <FormControl>this.authForm.get('email');
   }
 
   get password(): FormControl{
     return <FormControl>this.authForm.get('password');
+  }
+
+  changeAuthAction(): void{
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Login' : 'Sing Up';
+    this.configs.actionChange = isSignIn ? 'create a account' : 'Alredy have a account';
+    !isSignIn ? this.authForm.addControl('name', this.nameControl) : this.authForm.removeControl('name');
   }
 
   onSubmit():void{
